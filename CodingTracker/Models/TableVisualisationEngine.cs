9fe8ef7e756d08ecb91fd1a5f5812bb0f58b6bbd
Eyad Mostafa <Eyad Mostafa@LAPTOP-4N2ROENB>
2024-08-19 02:33:29 +0300

    Add project files.

---
+++ CodingTracker/Models/TableVisualisationEngine.cs
@@ -0,0 +1,48 @@
+﻿using Spectre.Console;
+
+namespace CodingTracker.Models;
+
+internal static class TableVisualisationEngine
+{
+    internal static void PrintSessions(List<CodingSession> sessions)
+    {
+        Console.Clear();
+        var table = new Table();
+
+        table.Title("Coding Sessions");
+
+        table.AddColumn("ID");
+        table.AddColumn("Date");
+        table.AddColumn("Start Time");
+        table.AddColumn("End Time");
+        table.AddColumn("Duration");
+        table.AddColumn("Description");
+        foreach (var session in sessions)
+        {
+            table.AddRow($"{session.Id}", $"{session.Date}", $"{session.StartTime}", $"{session.EndTime}", $"{session.DurationFormatted}", $"{session.Description}");
+        }
+
+        AnsiConsole.Write(table);
+    }
+
+
+
+
+    //    // Display a progress bar
+    //    AnsiConsole.Progress()
+    //        .Start(ctx =>
+    //        {
+    //    var task = ctx.AddTask("[green]Processing...[/]");
+
+    //    while (!ctx.IsFinished)
+    //    {
+    //        task.Increment(12.5);
+    //        Thread.Sleep(50);
+    //    }
+    //});
+    //    TimeSpan test = DateTime.Now.AddHours(3) - DateTime.Now;
+    //AnsiConsole.MarkupLine($"[bold red] {((int)test.TotalHours)} [/]");
+
+
+
+}
